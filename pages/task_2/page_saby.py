8efaf37533a0488bac49d62218d0a61a0f8842f4
Sergeychuk Sergey <Sergeychuk.serzh@bk.ru>
2025-07-03 15:16:25 +0500

    begin write PageSaby

---
+++ pages/task_2/page_saby.py
@@ -0,0 +1,39 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from Tenzor_task.locators.selectors import TenzorLocators2
+
+
+class PageSaby:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def open_saby_contact(self):
+        self.driver.get('https://saby.ru/')
+        self.driver.find_element(By.XPATH, '//div[contains(text(), "Контакты")]').click()
+        self.driver.find_element(By.CSS_SELECTOR, '.sbisru-Header-ContactsMenu__arrow-icon').click()
+
+    def check_region(self, region):
+        pagination_block = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(
+            TenzorLocators2.LOCATOR_TENZOR_CHECK_REGION)).text
+        assert pagination_block == region
+
+    def check_city(self, city):
+        check_city = self.driver.find_element(By.XPATH, '//div[contains(text(), "Челябинск")]')
+        assert check_city.text == city
+
+    def list_partners(self):
+        self.driver.get('https://saby.ru/')
+        self.driver.find_element(By.XPATH, '//div[contains(text(), "Контакты")]').click()
+        self.driver.find_element(By.CSS_SELECTOR, '.sbisru-Header-ContactsMenu__arrow-icon').click()
+        list_partners = []
+        block_partners = WebDriverWait(self.driver, timeout=30).until(
+            EC.presence_of_all_elements_located(TenzorLocators2.LOCATOR_TENZOR_CHECK_PARTNERS))
+        for partner in block_partners:
+            partners = partner.text.split("\n")
+            if len(partners) > 1:
+                for elem in partners:
+                    list_partners.append(elem)
+        assert len(list_partners) > 0
--- test/test_task_tenzor_2.py
+++ test/test_task_tenzor_2.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -6,44 +8,35 @@ from Tenzor_task.locators.selectors import TenzorLocators2
 
 from selenium.webdriver.common.by import By
 
-
+from Tenzor_task.pages.task_2.page_saby import PageSaby
 
 
 class TestSaby():
+
     def test_check_region(self, driver):
-        driver.get('https://saby.ru/')
-        driver.find_element(By.XPATH, '//div[contains(text(), "Контакты")]').click()
-        driver.find_element(By.CSS_SELECTOR, '.sbisru-Header-ContactsMenu__arrow-icon').click()
-        pagination_block = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
-            TenzorLocators2.LOCATOR_TENZOR_CHECK_REGION)).text
-        assert pagination_block == 'Челябинская обл.'
+        page_saby = PageSaby(driver)
+        page_saby.open_saby_contact()
+        page_saby.check_region('Челябинская обл.')
 
     def test_check_city(self, driver):
-        """Проверяет что список партнеров не является пустым"""
-        driver.get('https://saby.ru/')
-        driver.find_element(By.XPATH, '//div[contains(text(), "Контакты")]').click()
-        driver.find_element(By.CSS_SELECTOR, '.sbisru-Header-ContactsMenu__arrow-icon').click()
-        check_city = driver.find_element(By.XPATH, '//div[@id="city-id-2"]')
-        assert check_city.text == 'Челябинск'
+        """Проверяет город"""
+        page_saby = PageSaby(driver)
+        page_saby.open_saby_contact()
+        page_saby.check_city('Челябинск')
 
     def test_list_partners(self, driver):
-        driver.get('https://saby.ru/')
-        driver.find_element(By.XPATH, '//div[contains(text(), "Контакты")]').click()
-        driver.find_element(By.CSS_SELECTOR, '.sbisru-Header-ContactsMenu__arrow-icon').click()
-        list_partners = []
-        block_partners = WebDriverWait(driver, timeout=30).until(EC.presence_of_all_elements_located(TenzorLocators2.LOCATOR_TENZOR_CHECK_PARTNERS))
-        for partner in block_partners:
-            partners = partner.text.split("\n")
-            if len(partners) > 1:
-                for elem in partners:
-                    list_partners.append(elem)
-        assert len(list_partners) > 0
+        page_saby = PageSaby(driver)
+        page_saby.open_saby_contact()
+        page_saby.list_partners()
 
     def test_change_region(self, driver):
         driver.get('https://saby.ru/')
         driver.find_element(By.XPATH, '//div[contains(text(), "Контакты")]').click()
         driver.find_element(By.CSS_SELECTOR, '.sbisru-Header-ContactsMenu__arrow-icon').click()
-        driver.find_element(By.XPATH, '// span[contains(text(), "Челябинская обл.")]').click()
+
+        driver.find_element(By.XPATH, '// span[@class="sbis_ru-Region-Chooser ml-16 ml-xm-0"]').click()
+
         driver.find_element(By.XPATH, '// span[contains(text(), "41 Камчатский край")]').click()
-        region = driver.find_element(By.XPATH, '// span[contains(text(), "41 Камчатский край")]')
-        assert region == "41 Камчатский край"
+        driver.find_element(By.XPATH, '(//span[@class="controls-DecoratorHighlight"])[43]').click()
+        assert driver.current_url == 'https://saby.ru/contacts/41-kamchatskij-kraj?tab=clients'
+        assert driver.title == 'Saby Контакты — Камчатский край'
